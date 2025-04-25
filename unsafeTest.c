b7de471f4932575a9534bc93a459745ca25f4786
Seb <bones@ucc.asn.au>
2025-04-25 19:08:48 +0800

    testing failfinder GH action

---
+++ unsafeTest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  char name[50];
+  char buffer[100];
+
+  printf("Enter your name: ");
+  gets(name);
+
+  strcpy(buffer, name);
+
+  printf("Hello, %s\n", buffer);
+
+  system("ls -la");
+
+  return 0;
+}
