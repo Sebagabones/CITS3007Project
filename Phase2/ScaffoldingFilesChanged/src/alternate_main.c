fa1d98ae6c40c9a69cd8df5c7987c52fcc96bb02
Seb <bones@ucc.asn.au>
2025-04-25 18:36:00 +0800

    Initial setup of Phase 2
    Added the scaffolding provided to our repo - check
    `Phase2/ScaffoldingFilesChanged/README` for more details about what I
    did lol

---
+++ Phase2/ScaffoldingFilesChanged/src/alternate_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#ifdef ALTERNATE_MAIN
+int main()
+{
+	printf("an alternative implementation of main\n");
+
+	return(0);
+}
+
+#endif
--- Phase2/ScaffoldingFilesChanged/update-compile-commands.sh
+++ Phase2/ScaffoldingFilesChanged/update-compile-commands.sh
@@ -3,11 +3,11 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # Log messages to ensure output is happening
-echo "Starting compile_commands.json generation script" >&2
+echo "Starting compile_commands.json generation script for ScaffoldingFilesChanged" >&2
 
 # Change to the repository root directory
-cd "$REPO_ROOT/src" || {
-  echo "Failed to change to repository src directory: $REPO_ROOT/src" >&2
+cd "$REPO_ROOT/Phase2/ScaffoldingFilesChanged/" || {
+  echo "Failed to change to repository src directory: $REPO_ROOT/Phase2/ScaffoldingFilesChanged/" >&2
   exit 1
 }
 
@@ -19,7 +19,7 @@ logicalCpuCount=$([ $(uname) = 'Darwin' ] && sysctl -n hw.logicalcpu_max || lscp
 if git diff --cached --name-only | grep -q -E '(Makefile|src/.*\.(c|cpp|h|hpp)|CMakeLists.txt)'; then
   echo "Source files or build files changed, regenerating compile_commands.json"
   make clean
-  bear -- make all no-sanitize debug thread full -j $logicalCpuCount
+  bear -- make -j $logicalCpuCount
   git add compile_commands.json
   make clean
 else
