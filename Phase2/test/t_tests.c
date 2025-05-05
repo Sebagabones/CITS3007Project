d7c6c73869eae0cede81657e5ffc451948988c6d
HenryGod69 <celestial69death@gmail.com>
2025-05-05 18:25:09 +0800

    Testing

--- Phase2/test/t_tests.c
+++ Phase2/test/t_tests.c
@@ -1,2 +1,8 @@
 #include "tst.h"
-#include "functions.h"
+
+tstsuite("Factorials") {
+    tstcheck(fact_0(1) == 1);
+    tstcheck(fact_0(2) == 2);
+    tstcheck(fact_0(3) == 6);
+    tstcheck(fact_0(5) == 120);
+}
