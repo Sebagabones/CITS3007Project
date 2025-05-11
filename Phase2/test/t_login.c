1a236c353aeed78b95f7673686a9f13b6ce3c23d
PFhuahua <peterfangau@gmail.com>
2025-05-11 21:43:21 +0800

    Added some session tests

--- Phase2/test/t_login.c
+++ Phase2/test/t_login.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include "tst.h"
 #include "account.h"
@@ -16,6 +17,7 @@ bool account_lookup_by_userid(const char *userid, account_t *acc)
 	{
 		strcpy(acc->userid, "Ichigo");
 		strcpy(acc->password_hash, "Bankai2");
+		acc->account_id       = 1;
 		acc->unban_time		  = 0;
 		acc->expiration_time  = 0;
 		acc->login_fail_count = 0;
@@ -57,24 +59,24 @@ tstsuite("handle_login")
 {
 	ip4_addr_t			 testip	  = 0;
 	time_t				 testtime = 100;
-	login_session_data_t testsession;
+	login_session_data_t testsession = {0};
 
-	tstcase("Username Empty")
+	tstcase("Username Empty Login")
 	{
 		tstcheck(handle_login("", "Bankai2", testip, testtime, -1, &testsession) == LOGIN_FAIL_USER_NOT_FOUND)
 	}
 
-	tstcase("Username Not Found")
+	tstcase("Username Not Found Login")
 	{
 		tstcheck(handle_login("Non-existent", "Bankai2", testip, testtime, -1, &testsession) == LOGIN_FAIL_USER_NOT_FOUND)
 	}
 
-	tstcase("Account Banned")
+	tstcase("Account Banned Login")
 	{
 		tstcheck(handle_login("Banned-Account", "Bankai3", testip, testtime, -1, &testsession) == LOGIN_FAIL_ACCOUNT_BANNED)
 	}
 
-	tstcase("Account Expired")
+	tstcase("Account Expired Login")
 	{
 		tstcheck(handle_login("Expired-Account", "Bankai2", testip, testtime, -1, &testsession) == LOGIN_FAIL_ACCOUNT_EXPIRED)
 	}
@@ -91,13 +93,29 @@ tstsuite("handle_login")
 		tstcheck(handle_login("Too-Many", "someBankai", testip, testtime, -1, &testsession) == LOGIN_FAIL_INTERNAL_ERROR)
 	}
 
-	tstcase("Password Wrong")
+	tstcase("Password Wrong Login + Session Check")
 	{
 		tstcheck(handle_login("Ichigo", "Bankai3", testip, testtime, -1, &testsession) == LOGIN_FAIL_BAD_PASSWORD)
+		tstcheck(testsession.account_id == 0)
+		tstcheck(testsession.session_start == 0)
+		tstcheck(testsession.expiration_time == 0)
 	}
 
-	tstcase("Correct Password")
+	tstcase("Correct Password Login + Session Check")
 	{
 		tstcheck(handle_login("Ichigo", "Bankai2", testip, testtime, -1, &testsession) == LOGIN_SUCCESS)
+		tstcheck(testsession.account_id == 1)
+		tstcheck(testsession.session_start == testtime)
+		tstcheck(testsession.expiration_time == testtime + 86400)
+	}
+
+	tstcase("Session Expiry Overflow")
+	{
+		login_session_data_t specsession;
+    	time_t spectime = INT_MAX - 86200;
+		tstcheck(handle_login("Ichigo", "Bankai2", testip, spectime, -1, &specsession) == LOGIN_SUCCESS)
+		tstcheck(specsession.account_id == 1)
+		tstcheck(specsession.session_start == spectime)
+		tstcheck(specsession.expiration_time > specsession.session_start) // Check for overflow
 	}
 }
